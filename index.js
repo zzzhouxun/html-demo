b1b52637b697809d34eaec4b5a36f0bf80bc967e
Arthur <1140735765@qq.com>
2020-10-12 16:59:52 +0800

    small

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
  * @Author: Arthur
  * @Date: 2020-10-12 16:11:22
  * @LastEditors: Arthur
- * @LastEditTime: 2020-10-12 16:20:26
+ * @LastEditTime: 2020-10-12 16:50:20
  * @Description: file content
  */
 function play() {
     console.log('paly audio');
     let playerDom = document.getElementById('player');
     playerDom.pause();
-    playerDom.src = '/static/audio/小丑.wav'
+    playerDom.src = './static/audio/小丑.wav'
     playerDom.play();
 }
