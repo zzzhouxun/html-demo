6fd07d666c0fc6df95b891ad09acb0182794678e
zz <572285331@qq.com>
2020-10-20 12:35:51 +0800

    init

--- index.js
+++ index.js
@@ -5,10 +5,58 @@
  * @LastEditTime: 2020-10-12 16:50:20
  * @Description: file content
  */
-function play() {
+function play1() {
     console.log('paly audio');
     let playerDom = document.getElementById('player');
     playerDom.pause();
-    playerDom.src = './static/audio/小丑.wav'
+    playerDom.src = './static/audio/伯斯效果.wav'
+    playerDom.play();
+}
+
+function play2() {
+    console.log('paly audio');
+    let playerDom = document.getElementById('player');
+    playerDom.pause();
+    playerDom.src = './static/audio/催眠合成贝司.wav'
+    playerDom.play();
+}
+
+function play3() {
+    console.log('paly audio');
+    let playerDom = document.getElementById('player');
+    playerDom.pause();
+    playerDom.src = './static/audio/方波.wav'
+    playerDom.play();
+}
+
+function play4() {
+    console.log('paly audio');
+    let playerDom = document.getElementById('player');
+    playerDom.pause();
+    playerDom.src = './static/audio/火星.wav'
+    playerDom.play();
+}
+
+function play5() {
+    console.log('paly audio');
+    let playerDom = document.getElementById('player');
+    playerDom.pause();
+    playerDom.src = './static/audio/第八奇迹.wav'
+    playerDom.play();
+}
+
+function play6() {
+    console.log('paly audio');
+    let playerDom = document.getElementById('player');
+    playerDom.pause();
+    playerDom.src = './static/audio/调频生成器.wav'
+    playerDom.play();
+}
+
+function play7() {
+    console.log('paly audio');
+    let playerDom = document.getElementById('player');
+    playerDom.pause();
+    playerDom.src = './static/audio/迹动画漩涡.wav'
     playerDom.play();
 }
